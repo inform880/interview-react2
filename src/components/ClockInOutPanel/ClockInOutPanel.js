46e8d430be917ccbfad110a54708c575b7e6a29d
inform880 <pop37corn@gmail.com>
2019-12-26 19:14:49 -0500

    Finished basic tests for clock in/out panel

--- src/components/ClockInOutPanel/ClockInOutPanel.js
+++ src/components/ClockInOutPanel/ClockInOutPanel.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const ClockInOutPanel = () => {
+const ClockInOutPanel = ({
+  createNewTask = () => null,
+  isExistingTaskInProgress = false,
+  existingTaskDescription = ""
+}) => {
+  const [isTaskInProgress, updateTaskInProgress] = useState(
+    isExistingTaskInProgress
+  );
+
+  const [taskDescription, updateTaskDescription] = useState(
+    existingTaskDescription
+  );
+
   return (
     <div>
+      {isTaskInProgress ? (
+        <div>
+          <p>{taskDescription}</p>
+          <button
+            onClick={() => {
+              updateTaskInProgress(false);
+              updateTaskDescription("");
+            }}
+          >
+            stop
+          </button>
+        </div>
+      ) : (
+        <div>
+          <input
+            value={taskDescription}
+            onChange={event => updateTaskDescription(event.target.value)}
+          />
+          <button onClick={() => updateTaskInProgress(true)}>start</button>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default ClockInOutPanel;
--- src/components/ClockInOutPanel/ClockInOutPanel.test.js
+++ src/components/ClockInOutPanel/ClockInOutPanel.test.js
@@ -1,23 +1,38 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import ClockInOutPanel from './ClockInOutPanel';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import ClockInOutPanel from "./ClockInOutPanel";
 
-test('renders text box and start button', () => {
-  const { getByText } = render(<ClockInOutPanel />);
+test("renders text box and start button", () => {
+  const { getByRole } = render(<ClockInOutPanel />);
 
-  expect(getByRole('button')).toHaveTextContent('start');
-  expect(getByRole('input')).toBeInTheDOM();
+  expect(getByRole("button")).toHaveTextContent("start");
+  expect(getByRole("textbox")).toBeInTheDocument();
 });
 
 test("start button switches to stop after filling in description and clicking button", () => {
-  const { getByText } = render(<ClockInOutPanel />);
+  const { getByText, getByRole } = render(<ClockInOutPanel />);
+  
+  fireEvent.change(getByRole("textbox"), {
+    target: { value: "test description" }
+  });
+  fireEvent.click(getByText("start"));
 
-  expect(getByRole('button')).toHaveTextContent('start');
+  expect(getByRole("button")).toHaveTextContent("stop");
 
-  fireEvent.click(getByText("Up"));  
-  fireEvent.change(input, { target: { value: 'test description' } })
+  expect(getByText("test description")).toBeInTheDocument();
+});
 
-  expect(getByRole('button')).toHaveTextContent('stop');
+test("After stop button is pressed, description is cleared and the input comes back", () => {
+  const { getByText, getByRole } = render(<ClockInOutPanel />);
+  
+  fireEvent.change(getByRole("textbox"), {
+    target: { value: "test description" }
+  });
+  fireEvent.click(getByText("start"));
+  fireEvent.click(getByText("stop"));
 
-  expect(getByText("test description")).toBeInTheDOM();
-})
+
+  expect(getByRole("button")).toHaveTextContent("start");
+  expect(getByRole("textbox")).toBeInTheDocument();
+  expect(getByRole("textbox").value).toBe("");
+});
